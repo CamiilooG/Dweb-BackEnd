836c0d50ac0ebbe8be2c087a99a72ebb9415903d
Camilo Gomez <camilo-gomez2@upc.edu.co>
2023-06-03 17:19:46 -0500

    add Endpoint getUnassignedPackages

--- src/controllers/package.controller.js
+++ src/controllers/package.controller.js
@@ -1,3 +1,4 @@
+import e from 'express'
 import { pool } from '../db.js'
 
 export const registerPackage = async (req, res) => {
@@ -8,4 +9,34 @@ export const registerPackage = async (req, res) => {
     res.status(200).json({
         message: `Paquete creado con tamalho: ${size}`
     })
+}
+export const getUnassignedPackages = async (req, res) => {
+    const queryPackages = `SELECT * from package WHERE state = 'Sin entregar'`
+    const [rows] = await pool.query(queryPackages)
+    // esta esperando a que se haga la consulta del nombre de usuario para retornar la info
+    const packages = await Promise.all(rows.map( async (packageElement) => {
+        const {locationFrom, locationTo, state, idpackage, iduser } = packageElement
+        const findUserPackage = `SELECT name FROM user WHERE iduser = '${iduser}'` 
+        const [rows] = await pool.query(findUserPackage)
+        // extrae del objeto el nombre
+        const name = rows[0]?.name
+        const info = {locationFrom, locationTo, state, idpackage,name }
+        return info
+    }))
+    res.json(packages)
+}
+export const getAssignedPackages = async (req, res) => {
+    const queryPackages = `SELECT * from package WHERE state = 'Entregado'`
+    const [rows] = await pool.query(queryPackages)
+    // esta esperando a que se haga la consulta del nombre de usuario para retornar la info
+    const packages = await Promise.all(rows.map( async (packageElement) => {
+        const {locationFrom, locationTo, state, idpackage, iduser } = packageElement
+        const findUserPackage = `SELECT name FROM user WHERE iduser = '${iduser}'` 
+        const [rows] = await pool.query(findUserPackage)
+        // extrae del objeto el nombre
+        const name = rows[0]?.name
+        const info = {locationFrom, locationTo, state, idpackage,name }
+        return info
+    }))
+    res.json(packages)
 }
--- src/routes/package.routes.js
+++ src/routes/package.routes.js
@@ -1,7 +1,8 @@
-import{registerPackage} from '../controllers/package.controller.js'
+import{registerPackage, getUnassignedPackages} from '../controllers/package.controller.js'
 import { Router } from 'express'
 const router = Router()
 
 router.post('/registerpackage', registerPackage)
+router.get('/getallpackages', getUnassignedPackages)
 
 export default router
